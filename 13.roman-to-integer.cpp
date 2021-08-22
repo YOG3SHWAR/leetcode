1778e40413c4714d99e71761c66e519a95251df7
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2021-08-22 14:51:30 +0530

    Roman to Integer

---
+++ 13.roman-to-integer.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=13 lang=cpp
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+
+using namespace std;
+
+class Solution
+{
+public:
+    int romanToInt(string s)
+    {
+        int ans = 0;
+        unordered_map<char, int> m;
+        m.insert(pair<char, int>('I', 1));
+        m.insert(pair<char, int>('V', 5));
+        m.insert(pair<char, int>('X', 10));
+        m.insert(pair<char, int>('L', 50));
+        m.insert(pair<char, int>('C', 100));
+        m.insert(pair<char, int>('D', 500));
+        m.insert(pair<char, int>('M', 1000));
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (m[s[i]] >= m[s[i + 1]])
+                ans += m[s[i]];
+            else
+                ans -= m[s[i]];
+        }
+        ans += m[s[s.length() - 1]];
+        return ans;
+    }
+};
+// @lc code=end
